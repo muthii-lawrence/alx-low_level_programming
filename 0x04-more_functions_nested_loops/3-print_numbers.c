5ab2f87832ef8604b9e3152b67138406fb29a868
muthii-lawrence <lao.m6275@gmail.com>
2023-02-23 06:38:50 -0800

    test 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#inlcude "main"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 28; c < 38; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
