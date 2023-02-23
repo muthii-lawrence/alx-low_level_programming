568a1c3cccea0935401ce57920883784bcc7a0b1
muthii-lawrence <lao.m6275@gmail.com>
2023-02-23 09:35:26 -0800

    Task-3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 inlcude "main"
 
 /**
- * print_numbers -  prints the numbers, from 0 to 9, followed by a new line
+ * print_numbers -  prints the numbers, from 0 to 9
  *
- * Return: Always 0
+ * Return: 0-9 folllowed by a new line
  */
 void print_numbers(void)
 {
