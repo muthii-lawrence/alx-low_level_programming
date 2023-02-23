c1004d012782c5243f744e6aa27d1db5ab0b0e54
muthii-lawrence <lao.m6275@gmail.com>
2023-02-23 06:26:20 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  mul - multiplies two integers
+ *  @a:a - Variable
+ *  @b:b - Variable
+ *  Return:  Always 0 success
+ */
+int mul(int a, int b)
+{
+int mul;
+
+mul = a * b;
+
+return (mul);
+}
