e73f8feeff975dc73550bbc07a8ec528bc03e7a6
muthii-lawrence <lao.m6275@gmail.com>
2023-02-23 06:41:52 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #inlcude "main"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9
+ * print_numbers -  checks for checks for a digit (0 through 9)
  * Return: Always 0
  */
 void print_numbers(void)
