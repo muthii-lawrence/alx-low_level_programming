a420fba126642a1dfe3f62042ea1d4e46e54ef43
muthii-lawrence <lao.m6275@gmail.com>
2023-02-27 04:19:34 -0800

    task03

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,15 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
+ * Return: string
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_puts(*str++);
+		_putchar(str[i]);
 	}
 		_putchar('\n');
 }
