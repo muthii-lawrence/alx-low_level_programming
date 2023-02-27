d698e0db1187c527a6a15fcb9199d5525e8a04e9
muthii-lawrence <lao.m6275@gmail.com>
2023-02-27 04:02:27 -0800

    task4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#inlude "main.h"
+
+/**
+ * puts -  a function that prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	while (str != '\0')
+	{
+		_putchar(str++);
+	}
+	_putchar('\n');
+}
