96010993582e3cc7f7bceb4fd357f25c302f5dd6
muthii-lawrence <lao.m6275@gmail.com>
2023-02-27 07:09:17 -0800

    task 09

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,10 +9,16 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+	int x = 0;
 
-	for (; src[i] != '\0'; i++)
+	while (*(scr + i) != '\0')
 	{
-		dest[i] = src[i];
+		i++;
 	}
+	for (; x < i; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[i] = '\n';
 	return (dest);
 }
