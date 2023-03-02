7806596b8191d354b65ee846211f6b710c503804
muthii-lawrence <lao.m6275@gmail.com>
2023-03-02 11:01:25 -0800

    task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,32 +1,35 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * rot13 - Entry point
- * ONE if, TWO loops only...
- * @n: input
- * Return: decrypted string
+ * rot13 - a function that encodes a string using rot13
+ * @s: The character to be checked
+ *
+ * Return: Return value
  */
-char *rot13(char *n)
+char *rot13(char *s)
 {
-	int x, rot_c = 13, i = 0;
-	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-		'Z', 'm', 'z'};
+	int a = 0;
 
-	while (n[i] != '\0')
+	while (s[a])
 	{
-		for (x = 0; x <= 51; x++)
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A'
+					&& s[a] <= 'Z'))
 		{
-			if (n[i] == toswap[x])
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M'
+						&& s[a] <= 'Z'))
 			{
-				n[i] = n[i] + rot_c;
-				x = 51;
+				s[a] -= 13;
+				break;
 			}
-			rot_c = rot_c * -1;
+
+			s[a] += 13;
+			break;
 		}
-		i++;
+
+		a++;
 	}
-	return (n);
+
+	return (s);
 }
+
