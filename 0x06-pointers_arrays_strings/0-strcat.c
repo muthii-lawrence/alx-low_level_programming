d0d5c34a7b4b6413de3f1fdc3f5fd93a3dd03d85
muthii-lawrence <lao.m6275@gmail.com>
2023-03-02 03:35:35 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,16 +4,17 @@
  * strcat - a function that concatenates two strings
  * @dest: copy to
  * @src: copy from
- * Return: two concentrated strings
+ * Return: two concat strings
  */
 
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
-	int i. n;
+	int i, n;
 
 	for (i = 0; dest[i] != '0\'; i++)
 	{
 	}
+
 	for (n = 0; (dest[i + n] = *src++) != '0\'; n++)
 	{
 	}
