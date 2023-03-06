039cfc1a6b5c2023d1fd380f575a0d41ba27c872
muthii-lawrence <lao.m6275@gmail.com>
2023-03-06 06:21:37 -0800

    task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,17 +5,17 @@
  * _strchr - Entry point
  * @s: input
  * @c: input
- * Return: Always 0
+ * Return: Always 0 (Success
  */
-
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i]; >= '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
 	}
+
 	return (NULL);
 }
