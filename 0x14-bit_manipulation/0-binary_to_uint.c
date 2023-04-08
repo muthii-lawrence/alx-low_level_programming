718a7c3809fade56c0297cf5989075c4bebbe7d2
muthii-lawrence <lao.m6275@gmail.com>
2023-04-08 11:20:18 -0700

    task 01

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,30 @@
 #include "main.h"
+
 /**
  * binary_to_uint - Entry Point
  * @b: const char
- * Return: 0 Always
+ * Return: 0
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0;
-	int base = 1, j = 0;
+	int base = 1, i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[j + 1])
+	while (b[i + 1])
 	{
-		if (b[j] != '0' && b[j] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		j++;
+		i++;
 	}
 
-	while (j >= 0)
+	while (i >= 0)
 	{
 		res += ((b[i] - '0') * base);
 		base *= 2;
-		j--;
+		i--;
 	}
 
 
