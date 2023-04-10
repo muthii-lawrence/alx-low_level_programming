52b61d28bcefeaf8d660e5c4af0edc0e714b721c
muthii-lawrence <lao.m6275@gmail.com>
2023-04-10 10:52:28 -0700

    task 2.2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * get_bit -  a function that returns the value of a bit at a given index
+ * @n: number to search for the bit
+ * @index: index
+ * Return: 0 Always
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+	return ((n & m) > 0);
+}
