2d8d8cbdc10a02d071a47842f9319e6cde43112c
muthii-lawrence <lao.m6275@gmail.com>
2023-07-06 22:54:00 -0700

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio>
+/**
+ * get_bit - Write a function that returns the value of a bit at a given index
+ * @n: input
+ * @index: index
+ * Return: 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+	return ((n & m) > 0);
+}
